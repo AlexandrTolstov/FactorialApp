420deba1c980d794ec907037fad8ee620880c30d
AlexandrTolstov <alextav_85@mail.ru>
2019-01-06 12:58:00 +0600

    Factorial

--- FactorialApp/Program.cs
+++ FactorialApp/Program.cs
@@ -10,6 +10,19 @@ namespace FactorialApp
     {
         static void Main(string[] args)
         {
+            if (args == null || args.Length < 1)
+                throw new ArgumentNullException("args");
+            int number = 0;
+            if (!Int32.TryParse(args[0], out number))
+                throw new ArgumentException("не возможно преобразовать строку в число");
+            if (number < 1)
+                throw new ArgumentException("число не должно быть меньше 1");
+            int result = 1;
+            for (int i = 0; i < number; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine("Факториал числа {0} равен {1}", number, result);
         }
     }
 }
